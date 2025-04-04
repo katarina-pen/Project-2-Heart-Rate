b330197e274f0ebd0d80ae68ffac0b92863df70f
katarina-pen <katarinapenava0711@gmail.com>
2025-04-04 11:35:55 +0200

    Piezo

--- heart-test/heart-test.ino
+++ heart-test/heart-test.ino
@@ -1,24 +1,30 @@
-const int ledPin = 8;
+const int ledPin = 3;
+const int buzzer = 8;
 
 void setup() {
   // initialize the serial communication:
   Serial.begin(9600);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
-  pinMode (ledPin, OUTPUT);
+  pinMode (ledPin, OUTPUT); //LED
+  pinMode(buzzer, OUTPUT); //Piezo
 
 }
 
 void loop() {
   
   if((digitalRead(10) == 1 )||(digitalRead(11) == 1)){
-   digitalWrite(ledPin, HIGH);
-   Serial.println('!');
+   digitalWrite(ledPin, HIGH); //LED turns on
+   tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(1000); // ... for 1 second
+   Serial.println('!'); //! prints in the serial monitor
   }
   else{
     // send the value of analog input 0:
       Serial.println(analogRead(A0));
-      digitalWrite(ledPin, LOW);
+      digitalWrite(ledPin, LOW); //LED turns off
+      noTone(buzzer);     // Stop sound...
+      delay(1000);         // ...for 1sec
   }
   //Wait for a bit to keep serial data from saturating
   delay(1);
